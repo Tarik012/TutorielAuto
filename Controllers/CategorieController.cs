b014ffe7bbf5687487a6d6397ca34268ed4c9f4a
Tarik012 <tc78200@gmail.com>
2022-06-02 16:13:18 +0200

    ajout des controlleurs

---
+++ Controllers/CategorieController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TutorielAuto.TutorielAuto.Models;
+
+namespace TutorielAuto.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private readonly TutorielAutoContext _context;
+
+        public CategorieController(TutorielAutoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categorie
+        public async Task<IActionResult> Index()
+        {
+              return _context.Categories != null ? 
+                          View(await _context.Categories.ToListAsync()) :
+                          Problem("Entity set 'TutorielAutoContext.Categories'  is null.");
+        }
+
+        // GET: Categorie/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categorie = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categorie == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorie);
+        }
+
+        // GET: Categorie/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorie/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Label")] Categorie categorie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categorie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categorie);
+        }
+
+        // GET: Categorie/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categorie = await _context.Categories.FindAsync(id);
+            if (categorie == null)
+            {
+                return NotFound();
+            }
+            return View(categorie);
+        }
+
+        // POST: Categorie/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Label")] Categorie categorie)
+        {
+            if (id != categorie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categorie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategorieExists(categorie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categorie);
+        }
+
+        // GET: Categorie/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categorie = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categorie == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorie);
+        }
+
+        // POST: Categorie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'TutorielAutoContext.Categories'  is null.");
+            }
+            var categorie = await _context.Categories.FindAsync(id);
+            if (categorie != null)
+            {
+                _context.Categories.Remove(categorie);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategorieExists(int id)
+        {
+          return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/TutorielController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TutorielAuto.TutorielAuto.Models;
+
+namespace TutorielAuto.Controllers
+{
+    public class TutorielController : Controller
+    {
+        private readonly TutorielAutoContext _context;
+
+        public TutorielController(TutorielAutoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tutoriel
+        public async Task<IActionResult> Index()
+        {
+            var tutorielAutoContext = _context.Tutoriels.Include(t => t.Categorie);
+            return View(await tutorielAutoContext.ToListAsync());
+        }
+
+        // GET: Tutoriel/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Tutoriels == null)
+            {
+                return NotFound();
+            }
+
+            var tutoriel = await _context.Tutoriels
+                .Include(t => t.Categorie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tutoriel == null)
+            {
+                return NotFound();
+            }
+
+            return View(tutoriel);
+        }
+
+        // GET: Tutoriel/Create
+        public IActionResult Create()
+        {
+            ViewData["CategorieId"] = new SelectList(_context.Categories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tutoriel/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titre,Dcc,Contenu,VideoLink,Dml,Type,CategorieId")] Tutoriel tutoriel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tutoriel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategorieId"] = new SelectList(_context.Categories, "Id", "Id", tutoriel.CategorieId);
+            return View(tutoriel);
+        }
+
+        // GET: Tutoriel/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Tutoriels == null)
+            {
+                return NotFound();
+            }
+
+            var tutoriel = await _context.Tutoriels.FindAsync(id);
+            if (tutoriel == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategorieId"] = new SelectList(_context.Categories, "Id", "Id", tutoriel.CategorieId);
+            return View(tutoriel);
+        }
+
+        // POST: Tutoriel/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titre,Dcc,Contenu,VideoLink,Dml,Type,CategorieId")] Tutoriel tutoriel)
+        {
+            if (id != tutoriel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tutoriel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TutorielExists(tutoriel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategorieId"] = new SelectList(_context.Categories, "Id", "Id", tutoriel.CategorieId);
+            return View(tutoriel);
+        }
+
+        // GET: Tutoriel/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Tutoriels == null)
+            {
+                return NotFound();
+            }
+
+            var tutoriel = await _context.Tutoriels
+                .Include(t => t.Categorie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tutoriel == null)
+            {
+                return NotFound();
+            }
+
+            return View(tutoriel);
+        }
+
+        // POST: Tutoriel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Tutoriels == null)
+            {
+                return Problem("Entity set 'TutorielAutoContext.Tutoriels'  is null.");
+            }
+            var tutoriel = await _context.Tutoriels.FindAsync(id);
+            if (tutoriel != null)
+            {
+                _context.Tutoriels.Remove(tutoriel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TutorielExists(int id)
+        {
+          return (_context.Tutoriels?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

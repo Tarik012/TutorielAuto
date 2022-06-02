820c7143a07fda2c7c366f524174fbe5451165a0
Tarik012 <tc78200@gmail.com>
2022-06-02 15:59:21 +0200

    ajout models via scaffolding

---
+++ Models/Categorie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorielAuto.TutorielAuto.Models
+{
+    public partial class Categorie
+    {
+        public Categorie()
+        {
+            Tutoriels = new HashSet<Tutoriel>();
+        }
+
+        public int Id { get; set; }
+        public string Label { get; set; } = null!;
+
+        public virtual ICollection<Tutoriel> Tutoriels { get; set; }
+    }
+}
---
+++ Models/Tutoriel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorielAuto.TutorielAuto.Models
+{
+    public partial class Tutoriel
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; } = null!;
+        public DateTime Dcc { get; set; }
+        public string Contenu { get; set; } = null!;
+        public string VideoLink { get; set; } = null!;
+        public DateTime Dml { get; set; }
+        public int Type { get; set; }
+        public int CategorieId { get; set; }
+
+        public virtual Categorie Categorie { get; set; } = null!;
+    }
+}

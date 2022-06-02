8a319a6e8d9779de8ebc852a8076e17bbded6848
Tarik012 <tc78200@gmail.com>
2022-06-02 23:28:45 +0200

    migration

---
+++ Migrations/20220602143804_MyfirstMigration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorielAuto.Migrations
+{
+    public partial class MyfirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Label = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorie", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tutoriel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titre = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    DCC = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Contenu = table.Column<string>(type: "text", nullable: false),
+                    VideoLink = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    DML = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CategorieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tutoriel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tutoriel_Categorie",
+                        column: x => x.CategorieId,
+                        principalTable: "Categorie",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tutoriel_CategorieId",
+                table: "Tutoriel",
+                column: "CategorieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tutoriel");
+
+            migrationBuilder.DropTable(
+                name: "Categorie");
+        }
+    }
+}
---
+++ TutorielAuto/Models/TutorielAutoContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TutorielAuto.TutorielAuto.Models
+{
+    public partial class TutorielAutoContext : DbContext
+    {
+        public TutorielAutoContext()
+        {
+        }
+
+        public TutorielAutoContext(DbContextOptions<TutorielAutoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categorie> Categories { get; set; } = null!;
+        public virtual DbSet<Tutoriel> Tutoriels { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categorie>(entity =>
+            {
+                entity.ToTable("Categorie");
+
+                entity.Property(e => e.Label)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Tutoriel>(entity =>
+            {
+                entity.ToTable("Tutoriel");
+
+                entity.Property(e => e.Contenu).HasColumnType("text");
+
+                entity.Property(e => e.Dcc)
+                    .HasColumnType("datetime")
+                    .HasColumnName("DCC");
+
+                entity.Property(e => e.Dml)
+                    .HasColumnType("datetime")
+                    .HasColumnName("DML");
+
+                entity.Property(e => e.Titre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.VideoLink)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Categorie)
+                    .WithMany(p => p.Tutoriels)
+                    .HasForeignKey(d => d.CategorieId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Tutoriel_Categorie");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
